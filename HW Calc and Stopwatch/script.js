7c963678c8c7239b3d77259aa28b97c73f069223
karinakomarovich <karina.komarovich@gmail.com>
2021-05-03 21:51:07 +0300

    Working with Calc

--- HW Calc and Stopwatch/script.js
+++ HW Calc and Stopwatch/script.js
@@ -1,98 +1,108 @@
-let
-    // calcNumbers = document.querySelectorAll('.calc-numbers'),
-    // calcOperators = document.querySelectorAll('.calc-operators'),
-    calcOutput = document.querySelector('.calc-output'),
+let calcOutput = document.querySelector('.calc-output'),
     thAll = document.querySelectorAll('th'),
     number = 0,
-    numberOne = 0,
-    numberTwo = 0,
     textOutput = '',
     result = 0,
     operator = '',
-    operators = [''],
-    allItems = [''],
-    textLength;
+    allItems = ['0'],
+    textOutputArray,
+    comma = false,
+    min,
+    operatorOne;
 thAll.forEach((item) =>{
     let addText = function(){
         let className = item.className;
+        let numberOne;
+        let numberTwo;
 
+        // For numbers 0-9 + ','
         if(className === 'calc-numbers'){
             number = this.dataset.id;
-            if (number === 'comma'){number = ','}
-            numberOne = "numberOne" + number;
+            if (number === 'comma'){
+            	number = '.';
+            	if (textOutput === ''){
+            		comma=true;
+            		textOutput = "0" + number;
+            	}
+        		else{textOutput = textOutput + number;}
+            }
+            else{
+            	textOutput = textOutput + number;}
             allItems.push(number);
-            textOutput = textOutput + number;
             calcOutput.innerHTML = `${textOutput}`;
-
         }
 
+        // For operators (!%)
         if(className === 'calc-operators'){
 
             operator = this.dataset.id;
-            operators.push(operator);
-            allItems.push(operator);
-            console.log(allItems);
-            console.log(operators);
 
-            if(operator === 'c'){
-                calcOutput.innerHTML = '';
-                textOutput = '';
-                result = 0;
-                operators = [''];
-                allItems = [''];
+			if(operator === undefined){return} // for %
+
+			if(operator === 'c'){
+                clearingCalc();
                 return;
             }
 
             if(operator === 'delete'){
-                textLength = textOutput.length -1;
-                calcOutput.innerHTML = `${textOutput.substring(0,textLength)}`;
-                textOutput.length = textOutput.length - 1;
-                console.log(textOutput.length);
+                deleteCalc();
                 return
-                }
-
-            textOutput = textOutput + operator;
-            calcOutput.innerHTML = `${textOutput}`;
-
-            if (operator === '='){
-                operators.forEach(() => {switch(item) {
-                    case '/':
-                        result = numberOne / numberTwo;
-                        break;
-                    case 'x':
-                        result = numberOne * numberTwo;
-                        break;
-                    case '-':
-                        result = numberOne - numberTwo;
-                        break;
-                    case '+':
-                        result = numberOne + numberTwo;
-                        break;
-                    default:
-                        result = 0;
-                }});
-                operators = [''];
-                allItems = [result];
-                textOutput = textOutput + result;
-                calcOutput.innerHTML = `${textOutput}`;
             }
 
+            allItems.push(operator);
+
+            if (operator === '='){
+
+                if(comma){
+                    numberOne = Number(textOutput.substring(0,min)); // min = index of operator
+                    max= allItems.indexOf(operator,min); // max = index of operator '='
+                    numberTwo = Number(textOutput.substring(min,max));
+                } else{
+                    numberOne = Number(textOutput.substring(0,(min-1)));
+                    max = allItems.indexOf(operator,min);
+                    numberTwo = Number(textOutput.substring(min,max-1));
+                }
+
+				switch(operatorOne) {
+                    case '/': result = numberOne / numberTwo; break;
+                    case '*': result = numberOne * numberTwo; break;
+                    case '-': result = numberOne - numberTwo; break;
+                    case '+': result = numberOne + numberTwo; break;
+                    default: result = 0;
+                }
+                result = +result.toFixed(2);
+                allItems = [result];
+                textOutput = result;
+            } else{
+            	cointerNumber();
+            	operatorOne = operator;
+                textOutput = textOutput + operator;
+            }
+            // add text of HTM
+            calcOutput.innerHTML = `${textOutput}`;
         }
 
-
-        // textOutput = numberOne + operator + numberTwo;
-        // calcOutput.innerHTML = `${textOutput}`;
-
-        // result = result + operator;
-        // if (number === '='){calcOutput.innerHTML = `${result}`}
     };
 
-
     item.addEventListener('click', addText);
-})
+});
+let clearingCalc = function(){
+	calcOutput.innerHTML = '';
+    textOutput = '';
+    result = 0;
+    allItems = [''];
+};
+let deleteCalc = function(){
+	if(textOutput.split === undefined){return} // fix error when delete result
+	textOutputArray = textOutput.split('');
+	textOutputArray.pop(); // delete item from the end
+	textOutput = textOutputArray.join('');
+	calcOutput.innerHTML = `${textOutput.substring(0, textOutput.length)}`;
+};
+let cointerNumber = function(){
+     min = allItems.indexOf(operator,0);
+};
 
-
-calcOutput.innerHTML = `${textOutput.substring(0, textOutput.length - 1)}`;
 
 
 // calcNumbers.forEach((item) =>{
