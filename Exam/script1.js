9e4dba26b0b6d4a1f393a16f1cce1785aa5eb4a8
karinakomarovich <karina.komarovich@gmail.com>
2021-10-20 19:53:46 +0300

    feat: js exam

---
+++ Exam/script1.js
@@ -0,0 +1,73 @@
+const cells = document.querySelectorAll('.cells');
+let i = 0;
+for(let item of cells){
+    item.setAttribute('id', i);
+    i++;
+}
+cells.forEach((item) => {
+    item.addEventListener('load', checkStorage);
+    item.addEventListener('click', borderUp );
+    item.addEventListener('dblclick', editCell);
+    item.addEventListener('change', saveText);
+});
+function checkStorage() {
+    for(let i of cells){
+        // console.log(JSON.parse(localStorage.getItem(i.id)));
+        // console.log(validDate);
+        // console.dir(i)
+        // if ( === 'yellow') {
+        //     i.children[0].style.background === 'yellow'
+        // }
+
+        // console.log(JSON.parse(localStorage.getItem(i.id)))
+    }
+}
+function borderUp(event) {
+    event.target.style.cursor = 'default';
+    event.target.readOnly = true;
+    event.preventDefault();
+    cells.forEach( (item) => {
+        item.classList.remove('bolder')
+    });
+    event.target.parentElement.classList.add('bolder')
+}
+function editCell(event) {
+    event.target.readOnly = false;
+}
+let item = {};
+function saveText(event) {
+    // console.dir(event.target);
+    event.target.style.background = 'yellow';
+    event.target.readOnly = false;
+    let date = new Date;
+    let dateParse = Date.parse(date);
+    let parentID = event.target.parentElement.getAttribute('id');
+    let value = event.target.value;
+    // console.dir(event.target);
+    let item = {
+        date: JSON.stringify(dateParse),
+        value: value,
+    };
+    localStorage.setItem(parentID, JSON.stringify(item));
+}
+function timer(){
+    setInterval(checkTimer, 2000)
+}
+function checkTimer(){
+    for(let i of cells){
+        // console.log(JSON.parse(localStorage.getItem(i.id)));
+        // console.log(validDate);
+        // console.dir(i)
+        let newDate = new Date;
+        let validDate = Date.parse(newDate) - 100;
+        let itemDate = i.id[date];
+        console.log(itemDate);
+        if (JSON.parse(localStorage.getItem(i.id[date]))<validDate) {
+            i.children[0].style.background = 'transparent';
+            console.log(JSON.parse(localStorage.getItem(i.id)))
+        }
+
+        // console.log(JSON.parse(localStorage.getItem(i.id)))
+    }
+}
+timer();
